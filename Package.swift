d2945f84c840bf87a7a9e691968891c4ca0d871b
Josh Kaplan <josh@joshkaplan.com>
2021-10-11 22:54:18 +1300

    Specifies macOS 10.10 as the minimum supported platform

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SecureXPC",
+    platforms: [.macOS(.v10_10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
