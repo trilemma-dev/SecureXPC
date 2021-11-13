440c4e6497fad6a405fba4d96746c30209013513
Alexander Momchilov <alexandermomchilov@gmail.com>
2021-11-13 15:24:51 -0500

    Persist client connection

--- Sources/SecureXPC/Client/XPCMachClient.swift
+++ Sources/SecureXPC/Client/XPCMachClient.swift
@@ -12,10 +12,6 @@ import Foundation
 /// In the case of this framework, the Mach service is expected to be represented by an `XPCMachServer`.
 internal class XPCMachClient: XPCClient {
     /// Creates and returns a connection for the Mach service represented by this client.
-    ///
-    /// > Note: This client implementation intentionally does not store a reference to the ``xpc_connection_t`` as it can become
-    /// invalid for numerous reasons. Since it's expected relatively few messages will be sent and the lowest possible
-    /// latency isn't needed, it's simpler to always create the connection on demand each time a message is to be sent.
     internal override func createConnection() -> xpc_connection_t {
         xpc_connection_create_mach_service(self.serviceName, nil, 0)
     }
--- Sources/SecureXPC/Client/XPCServiceClient.swift
+++ Sources/SecureXPC/Client/XPCServiceClient.swift
@@ -12,10 +12,6 @@ import Foundation
 /// In the case of this framework, the XPC Service is expected to be represented by an `XPCServiceServer`.
 internal class XPCServiceClient: XPCClient {
     /// Creates and returns a connection for the XPC service represented by this client.
-    ///
-    /// > Note: This client implementation intentionally does not store a reference to the ``xpc_connection_t`` as it can become
-    /// invalid for numerous reasons. Since it's expected relatively few messages will be sent and the lowest possible
-    /// latency isn't needed, it's simpler to always create the connection on demand each time a message is to be sent.
     internal override func createConnection() -> xpc_connection_t {
         xpc_connection_create(self.serviceName, nil)
     }
