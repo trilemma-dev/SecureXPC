1df2f68b88b0708349aa6728098e77c307f6b026
Josh Kaplan <josh@joshkaplan.com>
2022-02-23 23:16:38 +1300

    Adds ability to use an async error handler with `XPCServer`
    Also added tests to validate that the server's error handler actually gets called.

---
+++ Tests/SecureXPCTests/Client & Server/Server Error Handler Test.swift
@@ -0,0 +1,80 @@
+//
+//  Server Error Handler Test.swift
+//  
+//
+//  Created by Josh Kaplan on 2022-02-23.
+//
+
+import Foundation
+import XCTest
+import SecureXPC
+
+// Note: it's intentional this is *not* `Codable` to validate that server side all errors are sent to the error handler
+private enum ExampleError: Error, Equatable {
+    case completeAndUtterFailure
+}
+
+class ServerErrorHandlerTest: XCTestCase {
+    
+    func testErrorHandler_Sync()  throws {
+        let errorToThrow = ExampleError.completeAndUtterFailure
+        let errorExpectation = self.expectation(description: "\(errorToThrow) should be provided to error handler")
+        
+        let failureRoute = XPCRoute.named("always", "throws")
+        let server = XPCServer.makeAnonymous()
+        let client = XPCClient.forEndpoint(server.endpoint)
+        server.registerRoute(failureRoute) {
+            throw ExampleError.completeAndUtterFailure
+        }
+        server.setErrorHandler { error in
+            switch error {
+                case .handlerError(let error):
+                    if case let .available(underlyingError) = error.underlyingError,
+                       underlyingError as? ExampleError == errorToThrow {
+                        errorExpectation.fulfill()
+                    } else {
+                        XCTFail("Unexpected underlying error: \(error)")
+                    }
+                default:
+                    XCTFail("Unexpected error: \(error)")
+            }
+        }
+        
+        server.start()
+        
+        client.send(route: failureRoute, onCompletion: nil)
+        
+        self.waitForExpectations(timeout: 1)
+    }
+    
+    func testErrorHandler_Async() throws {
+        let errorToThrow = ExampleError.completeAndUtterFailure
+        let errorExpectation = self.expectation(description: "\(errorToThrow) should be provided to error handler")
+        
+        let failureRoute = XPCRoute.named("always", "throws")
+        let server = XPCServer.makeAnonymous()
+        let client = XPCClient.forEndpoint(server.endpoint)
+        server.registerRoute(failureRoute) {
+            throw ExampleError.completeAndUtterFailure
+        }
+        server.setErrorHandler { (error: XPCError) async -> Void in
+            switch error {
+                case .handlerError(let error):
+                    if case let .available(underlyingError) = error.underlyingError,
+                       underlyingError as? ExampleError == errorToThrow {
+                        errorExpectation.fulfill()
+                    } else {
+                        XCTFail("Unexpected underlying error: \(error)")
+                    }
+                default:
+                    XCTFail("Unexpected error: \(error)")
+            }
+        }
+        
+        server.start()
+        
+        client.send(route: failureRoute, onCompletion: nil)
+        
+        self.waitForExpectations(timeout: 1)
+    }
+}
